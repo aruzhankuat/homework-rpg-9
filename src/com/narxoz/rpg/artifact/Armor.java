b151658d77cfffe9543363168bf2457028765019
aruzhankuat <aruzhan.kuat@narxoz.kz>
2026-05-07 23:54:08 +0500

    9

--- src/com/narxoz/rpg/artifact/Armor.java
+++ src/com/narxoz/rpg/artifact/Armor.java
@@ -1,23 +1,10 @@
 package com.narxoz.rpg.artifact;
 
-/**
- * A protective suit, plate, or enchanted shield.
- */
 public class Armor extends Artifact {
-
-    private final int defenseBonus;
-
-    public Armor(String name, int value, int weight, int defenseBonus) {
-        super(name, value, weight);
-        this.defenseBonus = defenseBonus;
-    }
-
-    public int getDefenseBonus() {
-        return defenseBonus;
-    }
+    public Armor(String name) { super(name); }
 
     @Override
     public void accept(ArtifactVisitor visitor) {
-        // TODO: call visitor.visit(this) for double dispatch.
+        visitor.visit(this);
     }
 }
