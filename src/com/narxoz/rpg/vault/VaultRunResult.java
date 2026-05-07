389a1c7843d9c92b54b200efcfed4ff7b7eaffec
aruzhankuat <aruzhan.kuat@narxoz.kz>
2026-05-08 00:07:06 +0500

    9

--- src/com/narxoz/rpg/vault/VaultRunResult.java
+++ src/com/narxoz/rpg/vault/VaultRunResult.java
@@ -1,38 +1,13 @@
 package com.narxoz.rpg.vault;
 
-/**
- * Summary of a Chronomancer's Vault run.
- */
 public class VaultRunResult {
+    private String status;
 
-    private final int artifactsAppraised;
-    private final int mementosCreated;
-    private final int restoredCount;
-
-    public VaultRunResult(int artifactsAppraised, int mementosCreated, int restoredCount) {
-        this.artifactsAppraised = artifactsAppraised;
-        this.mementosCreated = mementosCreated;
-        this.restoredCount = restoredCount;
+    public VaultRunResult(String status) {
+        this.status = status;
     }
 
-    public int getArtifactsAppraised() {
-        return artifactsAppraised;
-    }
-
-    public int getMementosCreated() {
-        return mementosCreated;
-    }
-
-    public int getRestoredCount() {
-        return restoredCount;
-    }
-
-    @Override
-    public String toString() {
-        return "VaultRunResult{"
-                + "artifactsAppraised=" + artifactsAppraised
-                + ", mementosCreated=" + mementosCreated
-                + ", restoredCount=" + restoredCount
-                + '}';
+    public void printResult() {
+        System.out.println("--- Результат забега: " + status + " ---");
     }
 }
